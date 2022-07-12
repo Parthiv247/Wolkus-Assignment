c984a42df7e97acb58c7491a61e76a7ae90cbd4e
Parthiv Patel <73586346+Parthiv247@users.noreply.github.com>
2022-07-12 19:35:57 +0530

    Add files via upload

---
+++ Wolkus Assignment/connectionn.php
@@ -0,0 +1,31 @@
+<?php
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+
+
+
+
+	$con = new mysqli('localhost','root','','phpdbs');
+	if($con->connect_error){
+		die("Failed to connect :".$con->connect_error);
+}else{
+	$stmt = $con->prepare("select * from register where email = ?");
+	$stmt->bind_param("s", $email);
+	$stmt->execute();
+	$stmt_result = $stmt->get_result();
+	if($stmt_result->num_rows > 0){
+		$data = $stmt_result->fetch_assoc();
+		if($data['password'] === $password){
+			//echo "<h2>login Success</h2>";
+			if(isset($_POST["submit1"]))
+            		{
+                		header('Location:webtest.html');
+            		}
+		}else {
+			echo "<h2>Invalid Email or password</h2>";
+		}
+	}else{
+		echo "<h2>Invalid Email or password</h2>";
+	}
+}
+?>
